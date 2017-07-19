d5fafd0ec2a2b0d31ca0e19df335190a4e7fa7d8
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-19 12:24:47 +0300

    Updated AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,10 +1,16 @@
 <?php
-
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Laravel\Dusk\DuskServiceProvider;
+use Illuminate\Support\ServiceProvider;
+use App\Manager\{UserManager, CarManager};
+use App\Manager\Contract\CarManager as CarManagerContract;
+use App\Manager\Contract\UserManager as UserManagerContract;
 
+/**
+ * Class AppServiceProvider
+ * @package App\Providers
+ */
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -12,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         //
     }
@@ -22,8 +28,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
+        // Registration of UserManager and CarManager
+        $this->app->bind(UserManagerContract::class, UserManager::class);
+        $this->app->bind(CarManagerContract::class, CarManager::class);
+
         if ($this->app->environment('local', 'testing')) {
             $this->app->register(DuskServiceProvider::class);
         }

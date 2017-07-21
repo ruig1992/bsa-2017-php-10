5ee52d2d709a862198dfafcc70555ad780391fcb
Ruslan Danylevskyj <ruigdan9240@gmail.com>
2017-07-21 13:33:51 +0300

    Fixed alert msg view

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,7 @@
 <div class="alert alert-{{ $type or 'danger' }}" role="alert">
   <i class="fa fa-frown-o fa-3x mr-3 align-middle" aria-hidden="true"></i>
 
-  @if ($errorCode or 0)
+  @if ($errorCode ?? 0)
     <b>Error {{ $errorCode }}
       <i class="fa fa-angle-right ml-1 mr-2" aria-hidden="true"></i></b>
   @endif
